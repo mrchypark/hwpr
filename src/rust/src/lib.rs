3eb54b14845d55501168491413c9e979ba640e7b
Chanyub.Park <mrchypark@gmail.com>
2023-07-28 05:59:23 +0900

    add readme rmd

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,9 +1,30 @@
+use extendr_api::prelude::*;
+use hwp::hwp::version::Version;
+
+#[derive(Clone)]
+pub struct RVersion(pub Version);
+
+#[extendr]
+impl RVersion {
+  fn majer(&self) -> u8 {
+      self.0.major
+  }
+  fn minor(&self) -> u8 {
+    self.0.minor
+  }
+  fn micro(&self) -> u8 {
+    self.0.micro
+  }
+  fn build_number(&self) -> u8 {
+    self.0.build_number
+  }
+  fn to_string(&self) -> String {
+    self.0.to_string()
+  }
+}
 
 
-// Macro to generate exports.
-// This ensures exported functions are registered with R.
-// See corresponding C code in `entrypoint.c`.
 extendr_module! {
-    mod hangeulr;
-    impl RVersion;
+  mod hangeulr;
+  impl RVersion;
 }
